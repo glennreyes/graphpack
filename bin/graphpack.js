a56d851f7dcdd8dd0fe56137dcf6e436266cb508
Glenn Reyes <glenn@glennreyes.com>
2018-10-16 13:59:43 +0200

    Add build command

---
+++ bin/graphpack.js
@@ -0,0 +1,41 @@
+#!/usr/bin/env node
+const nodemon = require('nodemon');
+const path = require('path');
+const { once } = require('ramda');
+const webpack = require('webpack');
+const paths = require('../config/paths');
+const webpackConfig = require('../config/webpack.config');
+
+const compiler = webpack(webpackConfig);
+
+const startDevServer = () => {
+  const serverPaths = Object.keys(compiler.options.entry).map(entry =>
+    path.join(compiler.options.output.path, `${entry}.js`),
+  );
+  compiler.watch(
+    webpackConfig.watchOptions,
+    once((error, stats) => {
+      if (error || stats.hasErrors()) {
+        throw Error(error || stats.errors);
+      }
+
+      nodemon({ script: serverPaths[0], watch: serverPaths }).on(
+        'quit',
+        process.exit,
+      );
+    }),
+  );
+};
+
+const createProductionBuild = () => {
+  compiler.run((error, stats) => {
+    if (error || stats.hasErrors()) {
+      throw Error(error || stats.errors);
+    }
+  });
+};
+
+require('yargs')
+  .command(['$0', 'dev'], 'Run development mode', {}, startDevServer)
+  .command('build', 'Create a production build', {}, createProductionBuild)
+  .argv;
