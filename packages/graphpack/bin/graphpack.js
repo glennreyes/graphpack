f64cf7712a223ea5ff73592bad9188037d182681
dotansimha <dotansimha@gmail.com>
2018-10-22 14:06:26 +0100

    added support for graphql-code-generator

--- packages/graphpack/bin/graphpack.js
+++ packages/graphpack/bin/graphpack.js
@@ -3,6 +3,7 @@ const nodemon = require('nodemon');
 const path = require('path');
 const { once } = require('ramda');
 const webpack = require('webpack');
+const { generate } = require('graphql-code-generator');
 const { loadWebpackConfig } = require('../config');
 
 const startDevServer = ({ compiler, config }) => {
@@ -32,17 +33,33 @@ const createProductionBuild = ({ compiler }) => {
   });
 };
 
+const generateSchemaTypings = (watch = false) => {
+  const srcPath = process.env.GRAPHPACK_SRC_DIR || `${process.cwd()}/src/`;
+  const schemaFilesRegex = path.join(srcPath, './**/*.graphql');
+  
+  generate({
+    schema: schemaFilesRegex,
+    template: 'graphql-codegen-typescript-template',
+    out: path.join(srcPath, 'graphql-typings.ts'),
+    args: [],
+    watch,
+    overwrite: true,
+  });
+}
+
 const startGraphPack = async () => {
   const config = await loadWebpackConfig();
   const compiler = webpack(config);
 
   require('yargs')
-    .command(['$0', 'dev'], 'Start graphpack dev server', {}, () =>
-      startDevServer({ compiler, config }),
-    )
-    .command('build', 'Create production build', {}, () =>
-      createProductionBuild({ compiler }),
-    ).argv;
+    .command(['$0', 'dev'], 'Start graphpack dev server', {}, () => {
+      generateSchemaTypings(true);
+      startDevServer({ compiler, config });
+    })
+    .command('build', 'Create production build', {}, () => {
+      generateSchemaTypings(false);
+      createProductionBuild({ compiler });
+    }).argv;
 };
 
 startGraphPack();
