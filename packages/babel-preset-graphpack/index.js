6f4401dbee69354dfface61918ea8ab1d0bbc156
Glenn Reyes <glenn@glennreyes.com>
2018-10-28 17:34:09 +0100

    Cache babel

--- packages/babel-preset-graphpack/index.js
+++ packages/babel-preset-graphpack/index.js
@@ -1,7 +1,11 @@
-module.exports = () => ({
-  plugins: ['@babel/plugin-syntax-dynamic-import'],
-  presets: [
-    ['@babel/preset-env', { targets: { node: 'current' }, modules: false }],
-    '@babel/preset-typescript',
-  ],
-});
+module.exports = api => {
+  api.cache(true);
+
+  return {
+    plugins: ['@babel/plugin-syntax-dynamic-import'],
+    presets: [
+      ['@babel/preset-env', { targets: { node: 'current' }, modules: false }],
+      '@babel/preset-typescript',
+    ],
+  };
+};
